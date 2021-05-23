8ccfa0733684e74662092d01421964f14a0e2007
Horlando <horlandoleao8@gmail.com>
2021-05-22 23:39:45 -0300

    pagina index concluida

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Encurtador de URL' });
+});
+
+/* GET home page. */
+router.post('/new', function(req, res, next) {
+  const url = req.body.url;
+  console.log(url)
+  res.send(url)
+});
+
+module.exports = router;
