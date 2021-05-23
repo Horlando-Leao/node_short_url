6abd74ea1c0b0d7b6b60a0a2cbf8aa9383448fcd
Horlando <horlandoleao8@gmail.com>
2021-05-23 13:52:36 -0300

    Tela de status feita com sucesso e redirecionamentos

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,30 @@ var express = require('express');
 var router = express.Router();
 const Link = require('../models/links');
 
+router.get('/:code/stats', async(req, res, next) =>{
+  const code = req.params.code;
+
+  const result = await Link.findOne({where: {code}})
+  if(!result){
+    return res.sendStatus(404);
+  }
+  res.render('stats', result.dataValues);
+});
+
+
+
+router.get('/:code', async (req, res, next) =>{
+  const code = req.params.code;
+
+  const result = await Link.findOne({where: {code}})
+  if(!result){
+    return res.sendStatus(404);
+  }
+  result.hits++;
+  await result.save();
+
+  res.redirect(result.url);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,20 +36,17 @@ router.get('/', function(req, res, next) {
 router.post('/new', async (req, res, next) => {
   const url = req.body.url;
 
-  const domain = 'http://localhost:3000/';
+  //const domain = 'http://localhost:3000/';
   const code = generateURL();
   
   const result = await Link.create({
     url,
     code
   });
-
-  res.render('stats', result);
-  //res.send(domain+code);
+  
+  res.render('stats', result.dataValues);
+  //res.send(result.dataValues)
 });
-
-
-
 
 
 //FUNCTIONS
