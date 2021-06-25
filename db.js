4bc413a972963cd3ba8c3f04adfb3cd59a805c91
Horlando <horlandoleao8@gmail.com>
2021-06-25 14:11:04 -0300

    update config in database for heroku

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'ec2-3-226-134-153.compute-1.amazonaws.com',
-    username: 'kmnzckzvdxfobw',
-    port: 5432,
-    database: 'd9gt4qieavduc4',
-    password: '31d608881eb7e29af61821a6c07ac7f294059fd304f2f48b0e8f78779efcbc32'
-});
+
+sequelize = null;
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres'
+    })
+}
+
+// const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     host: 'ec2-3-226-134-153.compute-1.amazonaws.com',
+//     username: 'kmnzckzvdxfobw',
+//     database: 'd9gt4qieavduc4',
+//     password: '31d608881eb7e29af61821a6c07ac7f294059fd304f2f48b0e8f78779efcbc32'
+// });
 
 module.exports = sequelize;
