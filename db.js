e0259b707504b8b3308168e640c36a2c14c5fd24
Horlando <horlandoleao8@gmail.com>
2021-06-25 13:54:28 -0300

    update config in database for heroku

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    dialect: 'postgres',
+    host: 'ec2-3-226-134-153.compute-1.amazonaws.com',
+    username: 'kmnzckzvdxfobw',
+    port: 5432,
+    database: 'd9gt4qieavduc4',
+    password: '31d608881eb7e29af61821a6c07ac7f294059fd304f2f48b0e8f78779efcbc32'
 });
-
 
 module.exports = sequelize;
